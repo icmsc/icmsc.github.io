c986fafdff2c639d46f747ab15e6837e1f4007ca
Douglas Sousa <89705994+douglasuw@users.noreply.github.com>
2024-01-01 15:47:13 -0300

    Correção grupo

--- campinas/grupo/script.js
+++ campinas/grupo/script.js
@@ -131,7 +131,7 @@ const meses = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Ag
 
 const dataBase = new Date(2023, 11, 31);
 		const dataBaseString = "31/12/2023";
-		const grupoBase = 5;
+		const grupoBase = 4;
 		const diasDaSemana = [0, 3, 4, 6];
 		const grupos = ["G1 - Eduardo", "G2 - Jovane", "G3 - Rodrigo", "G4 - Aliel", "G5 - Ricardo"];
 
